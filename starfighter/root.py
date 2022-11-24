a806698c71b3fa62cd121331bc9522cfd7a4d750
Sl1mmy <noebousquet2004@gmail.com>
2022-11-24 11:13:32 -0500

    added Root class

---
+++ starfighter/root.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+class Root(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title = "Starfighter"
+        # self.iconimage =
+
+        self.frame_menu = tk.Frame(self, background="white")
+        self.frame_jeu = tk.Frame(self, background="white")
+        # self.menu =
+        # self.jeu =
