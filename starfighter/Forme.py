3f9ee6f172cec28b98c9f424d49cbe059ac4a56b
Sl1mmy <noebousquet2004@gmail.com>
2022-11-24 11:28:32 -0500

    added Forme class

---
+++ starfighter/Forme.py
@@ -0,0 +1,22 @@
+class Forme:
+    def __init__(self):
+        self.width
+        self.height
+        self.x
+        self.y
+        self.color
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def get_color(self):
+        return self.color
